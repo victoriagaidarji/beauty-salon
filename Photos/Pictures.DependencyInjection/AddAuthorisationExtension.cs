e3041744a616240ab6c9143b7f0c370115b62565
Victoria Gaidarji <yourvictoris@gmail.com>
2025-03-21 14:42:43 +0200

    Add files via upload

---
+++ Photos/Pictures.DependencyInjection/AddAuthorisationExtension.cs
@@ -0,0 +1,31 @@
+﻿namespace Pictures.DependencyInjection;
+
+public static partial class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddAuthorizationExtension(this IServiceCollection service, IConfiguration config)
+    {
+        ArgumentNullException.ThrowIfNull(config);
+        var authOptions = config
+            .GetSection(nameof(AuthOptions))
+            .Get<AuthOptions>(options => options.BindNonPublicProperties = true);
+        service.AddSingleton(authOptions);
+        service.AddSingleton<ITokenProvider>(t => new TokenProvider(authOptions));
+        service.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = authOptions.ValidateIssuer,
+                    ValidIssuer = authOptions.Issuer,
+                    ValidateAudience = authOptions.ValidateAudience,
+                    ValidAudience = authOptions.Audience,
+                    ValidateLifetime = authOptions.ValidateLifetime,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authOptions.Key)),
+                    ValidateIssuerSigningKey = authOptions.ValidateIssuerSigningKey,
+                    RequireExpirationTime = authOptions.RequireExpirationTime
+                };
+            });
+        service.AddAuthorization(); 
+        return service;
+    }
+}
